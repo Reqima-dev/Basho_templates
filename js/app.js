ff9c69214af2cd6ab02aa604efc995820c6f3183
Adlou KONDO <104492843+Reqima-dev@users.noreply.github.com>
2022-05-19 08:59:55 +0100

    ajout du templates Bahso

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2.5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+
+
+
+const navItems = document.querySelector(".menu")
+const openNavBtn = document.querySelector("#open__nav-btn")
+const closeNavBtn = document.querySelector("#close__nav-btn")
